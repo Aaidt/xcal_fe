2ef817fe054945781bdf51bc93f349381ac57c54
Aadit <aadits2004@gmail.com>
2025-09-01 22:35:51 +0530

    persisting the shapes

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 
-
 import { Tldraw } from "tldraw"
 import "tldraw/tldraw.css"
 
 export default function Draw() {
   return (
     <div style={{ position: "fixed", inset: 0 }}>
-      <Tldraw />
+      <Tldraw persistenceKey="pages" />
     </div>
   )
 }
