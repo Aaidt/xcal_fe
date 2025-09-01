a516c939d5e331fecd4756a4776255802dbdfab1
Aadit <aadits2004@gmail.com>
2025-09-01 22:43:51 +0530

    using editor

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -1,10 +1,27 @@
 "use client"
 
 
-import { Tldraw } from "tldraw"
+import { Tldraw, toRichText } from "tldraw"
 import "tldraw/tldraw.css"
 
 export default function Draw() {
+  const handleMount = (editor: any) => {
+    editor.createShape({
+      type: 'text',
+      x: 200,
+      y: 200,
+      props: {
+        richText: toRichText("Hello world")
+      },
+    })
+
+    editor.selectAll()
+
+    editor.zoomToSelection({
+      animation: { duration: 5000 },
+    })
+  }
+
   return (
     <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw persistenceKey="pages" />
