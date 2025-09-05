4e6a63f2e1cb8e10b61ce9ba9a643f63321f6473
Aadit <aadits2004@gmail.com>
2025-09-05 15:43:43 +0530

    removing animations

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -5,26 +5,26 @@ import { Tldraw, toRichText } from "tldraw"
 import "tldraw/tldraw.css"
 
 export default function Draw() {
-  const handleMount = (editor: any) => {
-    editor.createShape({
-      type: 'text',
-      x: 200,
-      y: 200,
-      props: {
-        richText: toRichText("Hello world")
-      },
-    })
+    const handleMount = (editor: any) => {
+        // editor.createShape({
+        //   type: 'text',
+        //   x: 200,
+        //   y: 200,
+        //   props: {
+        //     richText: toRichText("Hello world")
+        //   },
+        // })
+        //
+        // editor.selectAll()
+        //
+        // editor.zoomToSelection({
+        //   animation: { duration: 5000 },
+        // })
+    }
 
-    editor.selectAll()
-
-    editor.zoomToSelection({
-      animation: { duration: 5000 },
-    })
-  }
-
-  return (
-    <div style={{ position: "fixed", inset: 0 }}>
-      <Tldraw persistenceKey="pages" onMount={handleMount} />
-    </div>
-  )
+    return (
+        <div style={{ position: "fixed", inset: 0 }}>
+            <Tldraw persistenceKey="pages" onMount={handleMount} />
+        </div>
+    )
 }
