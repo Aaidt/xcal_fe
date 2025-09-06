c75b73bd62cabcdc0f0869bdd1c9d5dd77c2af5a
Aadit <aadits2004@gmail.com>
2025-09-06 21:42:08 +0530

    login button ui

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
                         </Link>
                         <SignedOut>
                             <SignInButton>
-                                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                <button className="bg-foreground font-semibold text-background rounded-md font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                                     Login
                                 </button>
                             </SignInButton>
