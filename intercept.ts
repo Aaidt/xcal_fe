11aadbda77d3ae6c317506c280a79547f85c680e
Aadit <aadits2004@gmail.com>
2025-09-06 21:18:43 +0530

    removed some more unnneccessary files

--- intercept.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { useSession } from 'next-auth/react';
-
-axios.interceptors.request.use(config => {
-  const { data: session } = useSession();
-  //@ts-ignore
-  if (session?.user.id) config.headers.Authorization = `Bearer ${session?.user.id}`;
-  return config;
-});
